49e8c227bb2e90f89e5f461af387dbe7e604d81b
William Hayes Barber <barberhayes@gmail.com>
2025-08-26 17:41:32 -0400

    init (#1)

--- app/main.go
+++ app/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
 
 func main() {
-    fmt.Println("Hello ARES")
+    intervalStr := os.Getenv("INTERVAL_SECONDS")
+    intervalSeconds, err := strconv.Atoi(intervalStr)
+    if err != nil || intervalSeconds < 1 {
+        fmt.Println("Invalid interval, setting to default (30 seconds)")
+        intervalSeconds = 30
+    }
+    ticker := time.NewTicker(time.Duration(intervalSeconds) * time.Second)
+    defer ticker.Stop()
+    for t := range ticker.C {
+        fmt.Printf("Tick at %v\n", t)
+    }
 }
