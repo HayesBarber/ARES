046b17dfa29b7702e7b1907b40cf38beb0c0ce03
Hayes Barber <barberhayes@gmail.com>
2025-08-28 21:10:58 -0400

    parse function

--- app/main.go
+++ app/main.go
@@ -13,10 +13,41 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func parseEnvConfig() EnvConfig {
+	intervalStr := os.Getenv("INTERVAL_SECONDS")
+	intervalSeconds, err := strconv.Atoi(intervalStr)
+	if err != nil || intervalSeconds < 1 {
+		intervalSeconds = 30
+	}
+
+	baseURL := os.Getenv("BASE_URL")
+	if baseURL == "" {
+		baseURL = "http://localhost"
+	}
+
+	healthBody := os.Getenv("HEALTH_BODY")
+	if healthBody == "" {
+		healthBody = "{}"
+	}
+
+	timeoutStr := os.Getenv("HTTP_TIMEOUT_SECONDS")
+	httpTimeoutSeconds, err := strconv.Atoi(timeoutStr)
+	if err != nil || httpTimeoutSeconds < 1 {
+		httpTimeoutSeconds = 0
+	}
+
+	return EnvConfig{
+		IntervalSeconds:    intervalSeconds,
+		BaseURL:            baseURL,
+		HealthBody:         healthBody,
+		HTTPTimeoutSeconds: httpTimeoutSeconds,
+	}
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		fmt.Println("No .env file found, using environment variables")
+		fmt.Println("No .env file found")
 	}
 
 	intervalStr := os.Getenv("INTERVAL_SECONDS")
