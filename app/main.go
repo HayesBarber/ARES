960289c0b83c9ae9a2fdfafe68faefcf9d305021
Hayes Barber <barberhayes@gmail.com>
2025-08-30 20:29:18 -0400

    create config file

--- app/main.go
+++ app/main.go
@@ -6,43 +6,10 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os"
-	"strconv"
 	"time"
 
 	"github.com/joho/godotenv"
 )
-
-func parseEnvConfig() EnvConfig {
-	intervalStr := os.Getenv("INTERVAL_SECONDS")
-	intervalSeconds, err := strconv.Atoi(intervalStr)
-	if err != nil || intervalSeconds < 1 {
-		intervalSeconds = 30
-	}
-
-	baseURL := os.Getenv("BASE_URL")
-	if baseURL == "" {
-		baseURL = "http://localhost"
-	}
-
-	healthBody := os.Getenv("HEALTH_BODY")
-	if healthBody == "" {
-		healthBody = "{}"
-	}
-
-	timeoutStr := os.Getenv("HTTP_TIMEOUT_SECONDS")
-	httpTimeoutSeconds, err := strconv.Atoi(timeoutStr)
-	if err != nil || httpTimeoutSeconds < 1 {
-		httpTimeoutSeconds = 0
-	}
-
-	return EnvConfig{
-		IntervalSeconds:    intervalSeconds,
-		BaseURL:            baseURL,
-		HealthBody:         healthBody,
-		HTTPTimeoutSeconds: httpTimeoutSeconds,
-	}
-}
 
 func postHealthCheck(client *http.Client, url string, body string) (HealthResponse, error) {
 	var healthResp HealthResponse
