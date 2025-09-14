53fca1958ee6a70e515d38c5e636033e6f663495
Hayes Barber <barberhayes@gmail.com>
2025-09-14 18:17:40 -0400

    update script

--- start_prometheus.sh
+++ start_prometheus.sh
@@ -3,6 +3,6 @@ docker run -d \
   --restart unless-stopped \
   -p 9090:9090 \
   -v prometheus-volume:/prometheus \
-  -v "$(pwd)"/prometheus.yml:/etc/prometheus/prometheus.yml \
+  -v ./prometheus.yml:/etc/prometheus/prometheus.yml \
   prom/prometheus:latest \
   --config.file=/etc/prometheus/prometheus.yml
