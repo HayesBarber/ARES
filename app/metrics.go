a5df782c7d8f6af568b9efe1b1a303559eda6647
Hayes Barber <barberhayes@gmail.com>
2025-09-14 17:48:26 -0400

    define metrics

---
+++ app/metrics.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	healthyCount = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "service_healthy_total",
+			Help: "Total number of times the service was healthy",
+		},
+	)
+	moderateCount = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "service_moderate_total",
+			Help: "Total number of times the service was in moderate state",
+		},
+	)
+	unhealthyCount = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "service_unhealthy_total",
+			Help: "Total number of times the service was unhealthy",
+		},
+	)
+	missingDevicesCount = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "service_missing_devices_total",
+			Help: "Total number of times each device was missing",
+		},
+		[]string{"device"},
+	)
+	reasonCount = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "service_unhealthy_reason_total",
+			Help: "Total number of times the service was unhealthy for a given reason",
+		},
+		[]string{"reason"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(healthyCount)
+	prometheus.MustRegister(moderateCount)
+	prometheus.MustRegister(unhealthyCount)
+	prometheus.MustRegister(missingDevicesCount)
+	prometheus.MustRegister(reasonCount)
+}
