5567d38bdcf7d98bdb3a825ed1f70bf96e909ecb
Hayes Barber <barberhayes@gmail.com>
2025-09-14 19:46:29 -0400

    update script

--- start_prometheus.sh
+++ start_prometheus.sh
@@ -3,6 +3,7 @@
 docker run -d \
   --name prometheus \
   --restart unless-stopped \
+  --network host \
   -p 9090:9090 \
   -v prometheus-volume:/prometheus \
   -v ./prometheus.yml:/etc/prometheus/prometheus.yml \
